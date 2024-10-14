8ee9b0662c93582c853d5c3fe97a3eea67585f37
Endale2 <endale406@gmail.com>
2024-10-14 15:24:06 +0300

    bottom navbar is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flyx/base/bottom_nav_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme:
-              ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 3, 15, 244)),
-          useMaterial3: true,
-        ),
-        home: Text("hello world"));
+    return MaterialApp(home: BottomNavBar());
   }
 }
