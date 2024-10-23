0000e2fff2f21601fa346554d7780b46077cfa69
Endale2 <endale406@gmail.com>
2024-10-23 18:40:44 +0300

    updated

--- lib/base/screens/search/widgets/app_ticket_tabs.dart
+++ lib/base/screens/search/widgets/app_ticket_tabs.dart
@@ -5,7 +5,6 @@ class AppTicketTabs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Container(
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(50), color: Color(0xFFF6f4fd)),
